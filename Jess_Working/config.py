ccd7d21f7a440693ec96554255463387ea419f4f
nameyeh <nameyeh.cbits@gmail.com>
2018-03-21 13:05:13 -0500

    added folders: datasets (normal users, fitness users) and dataset_generators

--- Jess_Working/config.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = "rbmnPZ4BfLk5UUe3wLxXlHLpq"
-consumer_secret = "7HuGg9E8qQmMXqAo0ZM3relKlj5RFTB5nvfm64b54NcEyfFcKg"
-access_token = "971542032153022465-UdAQawHXtooKxkD5uRptUiOCIOy3hpc"
-access_token_secret = "nfuHBhIvsupY4cN3nP93wkwS6xA4e12odMOl7wPVJgiNZ"
